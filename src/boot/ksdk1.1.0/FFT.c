7d61a2e0678a4dbc5efc731a5c324185bda59fac
leacog <mikael.cognell@gmail.com>
2022-01-07 11:20:28 +0100

    added fft4

--- src/boot/ksdk1.1.0/FFT.c
+++ src/boot/ksdk1.1.0/FFT.c
@@ -7,7 +7,6 @@ void FFT(long complex * eightPoints, long complex * outArray){
 
   long complex x_ping[8];
   long complex x_pong[8];
-  //memcpy(&x_ping[0], eightPoints, 8);
 
   for(int i =0; i < 8; i++){
     x_pong[i] = eightPoints[BitReverseArray[i]]; 
@@ -16,40 +15,37 @@ void FFT(long complex * eightPoints, long complex * outArray){
   for(int i=0; i < 4; i++){
     FFT2(&x_pong[i*2], &x_ping[i*2]);
   }
-  /*
+  
   for(int i=0; i < 2; i++){
     FFT4(&x_ping[i*4], &x_pong[i*4]);
   }
-  */
-  //memcpy(outArray, &x_ping[0], 8);
+  
   for(int i =0; i<8; i++){
   	outArray[i] = x_ping[i];
   }
 }
 
 void FFT2(long complex * twoPoints, long complex * outArray){
-  static int BitReverseArray[2] = {0, 1};
-  //long complex x_rev[2];
-  long complex x[2];
-  //memcpy(&x_rev[0], twoPoints, 2);
-
-  for(int i =0; i < 2; i++){
-    x[i] = twoPoints[BitReverseArray[i]]; 
-  }
-  outArray[0] = x[0] + x[1];
-  outArray[1] = x[0] - x[1];
+  outArray[0] = twoPoints[0] + twoPoints[1];
+  outArray[1] = twoPoints[0] - twoPoints[1];
 }
+
+void FFT4(long complex * fourPoints, long complex * outArray){
+  long complex twiddleFactors [4] = { 1, 0-1*I, -1, 0+1*I};
+  outArray[0] = x[0] + x[2];
+  outArray[1] = x[1] + twiddleFactors[1] * x[1];
+  outArray[2] = x[0] - x[2];
+  outArray[3] = x[1] + twiddleFactors[4] * x[3];
+}
+
 /*
 void FFT4(long complex * fourPoints, long complex * outArray){
-  static int BitReverseArray[4] = {0, 1};
-  long complex x_rev[2];
-  long complex x[2];
-  memcpy(&x_rev[0], twoPoints, 2);
-
-  for(int i =0; i < 2; i++){
-    x[i] = x_rev[BitReverseArray[i]]; 
-  }
-  outArray[0] = x[0] + x[1];
-  outArray[1] = x[0] - x[1];
+  long complex twiddleFactors [4] = { 1, 0-1*I, -1, 0+1*I};
+  outArray[0] = x[0] + x[2];
+  outArray[1] = x[1] + twiddleFactors[1] * x[1];
+  outArray[2] = x[0] - x[2];
+  outArray[3] = x[1] + twiddleFactors[4] * x[3];
 }
 */
+
+
--- src/boot/ksdk1.1.0/FFT.h
+++ src/boot/ksdk1.1.0/FFT.h
@@ -2,3 +2,4 @@
 
 void FFT(long complex * eightPoints, long complex * outArray);
 void FFT2(long complex * twoPoints, long complex * outArray);
+void FFT4(long complex * fourPoints, long complex * outArray);
